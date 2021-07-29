1c889690af57a0ac0415b9e2a59e3dd1d6cf5524
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2021-07-29 15:09:12 -0300

    feat#Switch

---
+++ 12-Switch/switch.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+// retornar o dia da semana
+func diaDaSemana(numero int) string {
+	switch numero {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda-feira"
+	case 3:
+		return "Terça-feira"
+	case 4:
+		return "Quarta-feira"
+	case 5:
+		return "Quinta-feira"
+	case 6:
+		return "Sexta-feira"
+	case 7:
+		return "Sábado"
+	default:
+		return "Número inválido!"
+	}
+}
+
+// segunda maneira de usar o switch
+func diaDaSemana2(numero int) string {
+	switch {
+	case numero == 1:
+		return "Domingo"
+	case numero == 2:
+		return "Segunda-feira"
+	case numero == 3:
+		return "Terça-feira"
+	case numero == 4:
+		return "Quarta-feira"
+	case numero == 5:
+		return "Quinta-feira"
+	case numero == 6:
+		return "Sexta-feira"
+	case numero == 7:
+		return "Sábado"
+	default:
+		return "Número inválido"
+	}
+}
+
+func main() {
+	fmt.Println("Switch")
+
+	dia := diaDaSemana(4)
+	fmt.Println(dia)
+
+	dia2 := diaDaSemana2(1)
+	fmt.Println(dia2)
+}
