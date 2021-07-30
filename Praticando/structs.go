afc205c7db50e4220add248cbe9b775170c9f490
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2021-07-29 23:01:20 -0300

    feat#praticando structs

---
+++ Praticando/structs.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	age       uint8
+	active    bool
+}
+
+type profission struct {
+	name string
+	wage float32
+}
+
+func main() {
+	fmt.Println("Práticando Structs")
+
+	var u user
+	var pro profission
+
+	u.firstName = "Jonatas"
+	u.lastName = "Rosa Moura"
+	u.age = 21
+	u.active = true
+
+	pro.name = "Desenvolvedor Back-end de golang"
+	pro.wage = 7000
+
+	fmt.Println(
+		"Meu nome é", u.firstName, u.lastName, "e tenho", u.age, "anos de idade, sou", pro.name, "e ganho", pro.wage,
+	)
+
+	user2 := user{"Bruno", "Rosa Moura", 15, true}
+	user2Pro := profission{"Desenvolvedor back-end júnior de C#", 5000}
+	fmt.Println(user2.firstName, user2.lastName, "tem", user2.age, "anos de idade e ele é", user2Pro.name, "seu salário é de", user2Pro.wage)
+}
