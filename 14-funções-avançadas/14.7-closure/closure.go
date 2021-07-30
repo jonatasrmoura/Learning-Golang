c0590e734a478dc93a4e3fbe61fb5ff46e74a58b
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2021-07-30 18:44:19 -0300

    feat#funçoes closure

---
+++ 14-funções-avançadas/14.7-closure/closure.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Funções do tipo closure
+// São funções que referencião as variáveis que estão fora do seu corpo
+
+func closure() func() {
+	texto := "Dentro da função closure"
+
+	funcao := func() {
+		fmt.Println(texto)
+	}
+
+	return funcao
+}
+
+func main() {
+	texto := "Dentro da função main"
+	fmt.Println(texto)
+
+	funcaoNova := closure()
+	funcaoNova()
+}
